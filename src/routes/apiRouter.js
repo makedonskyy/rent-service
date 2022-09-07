ec0849c403a20496bb6b9670728bddea57021465
AynurMv <aynurmotigulov@gmail.com>
2022-09-07 16:37:00 +0300

    end initialization

---
+++ src/routes/apiRouter.js
@@ -0,0 +1,11 @@
+import express from 'express';
+// import { Student, Post } from '../db/models';
+
+const router = express.Router();
+
+// router.get('/students', async (req, res) => {
+// const allStudents = await Student.findAll();
+// res.json(allStudents);
+// });
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,23 +1,35 @@
 import express from 'express';
 import morgan from 'morgan';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Layout from './components/Layout';
+import session from 'express-session';
+import store from 'session-file-store';
+import indexRouter from './routes/indexRouter';
+import apiRouter from './routes/apiRouter';
 
-const PORT = 3000;
+require('dotenv').config();
+
+const PORT = process.env.SERVER_PORT || 3002;
 const app = express();
+const FileStore = store(session);
+
+const sessionConfig = {
+  name: 'user_sid', 				// Имя куки для хранения id сессии. По умолчанию - connect.sid
+  secret: process.env.SESSION_SECRET ?? 'test',	// Секретное слово для шифрования, может быть любым
+  resave: true, 				// Пересохранять ли куку при каждом запросе
+  store: new FileStore(),
+  saveUninitialized: false, 		// Создавать ли сессию без инициализации ключей в req.session
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 12, // Срок истечения годности куки в миллисекундах
+    httpOnly: true, 				// Серверная установка и удаление куки, по умолчанию true
+  },
+};
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session(sessionConfig));
 
-app.get('/', async (req, res) => {
-  const initState = { path: req.originalUrl };
-  const layoutComponent = React.createElement(Layout, { initState });
-  const html = renderToString(layoutComponent);
-  res.write('<!DOCTYPE html>');
-  res.end(html);
-});
+app.use('/', indexRouter);
+app.use('/api/v1', apiRouter);
 
 app.listen(PORT, () => console.log(`App has started on port ${PORT}`));
