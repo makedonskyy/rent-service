f410e6a3c78fd1bf2daa72e548518773a4668827
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-09-07 15:38:13 +0300

    initial commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './App';
+
+export default function Layout({ initState }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `window.initState=${JSON.stringify(initState)}`,
+          }}
+        />
+        <script defer src="/app.js" />
+        <script defer src="/vendor.js" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous" />
+        <title>Rent</title>
+      </head>
+      <body>
+        <div id="root">
+          <StaticRouter location={initState.path}>
+            <App {...initState} />
+          </StaticRouter>
+        </div>
+      </body>
+    </html>
+  );
+}
