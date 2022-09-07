c3eeefc742736c5e918ab34fa79209755ab7bb7a
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-09-07 19:15:59 +0300

    server fixed

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const FileStore = store(session);
 
 const sessionConfig = {
   name: 'user_sid', // Имя куки для хранения id сессии. По умолчанию - connect.sid
-  secret: process.env.SESSION_SECRET ?? 'test',	// Секретное слово для шифрования, может быть любым
+  secret: process.env.SESSION_SECRET ?? 'test', // Секретное слово для шифрования, может быть любым
   resave: true, // Пересохранять ли куку при каждом запросе
   store: new FileStore(),
   saveUninitialized: false, // Создавать ли сессию без инициализации ключей в req.session
