dd8c131348e74db7b6c95cdca4c022359041692b
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-09-09 21:43:06 +0300

    almost final

--- src/components/userInterface/EditCard.jsx
+++ src/components/userInterface/EditCard.jsx
@@ -26,46 +26,49 @@ export default function EditCard({ maApart }) {
     });
   };
   return (
-    <div style={{ width: '80%' }}>
-      <h2>Отредактировать описание жилья</h2>
-      <form onSubmit={submitHandler}>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlSelect1">Категория</label>
-          <select onChange={changeHandler} name="cathegory" value={form.cathegory} className="form-control" id="exampleFormControlSelect1">
-            <option value="2">Дом</option>
-            <option value="1">Квартира</option>
-            <option value="3">Комната</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlform1">Цена</label>
-          <form onChange={changeHandler} name="price" value={form.price} type="text" className="form-control" id="exampleFormControlform1" placeholder="Введите цену" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlSelect1">Количество комнат</label>
-          <select onChange={changeHandler} name="countOfRooms" value={form.countOfRooms} className="form-control" id="exampleFormControlSelect1">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlform1">Аддрес</label>
-          <form onChange={changeHandler} name="address" value={form.address} type="text" className="form-control" id="exampleFormControlform1" placeholder="Введите адресс" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlTextarea1">Описание</label>
-          <textarea onChange={changeHandler} name="description" value={form.description} className="form-control" id="exampleFormControlTextarea1" rows="3" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlform1">Ссылка на фото</label>
-          <form onChange={changeHandler} name="image" value={form.image} type="text" className="form-control" id="exampleFormControlform1" placeholder="Вставьте ссылку на фото" />
-        </div>
-        <button type="submit" value={form.submit} className="btn btn-dark">Отправить</button>
-      </form>
-      <Link to={`/myapartments/${apartId}`} className="btn btn-danger">Удалить</Link>
+
+    <div className="container">
+      <div className="col-6">
+        <h2>Отредактировать описание жилья</h2>
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlSelect1">Категория</label>
+            <select onChange={changeHandler} name="cathegory" value={form.cathegory} className="form-control" id="exampleFormControlSelect1">
+              <option value="2">Дом</option>
+              <option value="1">Квартира</option>
+              <option value="3">Комната</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlform1">Цена</label>
+            <form onChange={changeHandler} name="price" value={form.price} type="text" className="form-control" id="exampleFormControlform1" placeholder="Введите цену" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlSelect1">Количество комнат</label>
+            <select onChange={changeHandler} name="countOfRooms" value={form.countOfRooms} className="form-control" id="exampleFormControlSelect1">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlform1">Адрес</label>
+            <form onChange={changeHandler} name="address" value={form.address} type="text" className="form-control" id="exampleFormControlform1" placeholder="Введите адресс" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlTextarea1">Описание</label>
+            <textarea onChange={changeHandler} name="description" value={form.description} className="form-control" id="exampleFormControlTextarea1" rows="3" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlform1">Ссылка на фото</label>
+            <form onChange={changeHandler} name="image" value={form.image} type="text" className="form-control" id="exampleFormControlform1" placeholder="Вставьте ссылку на фото" />
+          </div>
+          <button type="submit" value={form.submit} className="btn btn-dark">Отправить</button>
+        </form>
+        <Link to={`/myapartments/${apartId}`} className="btn btn-danger">Удалить</Link>
+      </div>
     </div>
   );
 }
